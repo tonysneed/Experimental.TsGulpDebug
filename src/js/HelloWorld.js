694ee98bffb0318ce188050a27681672890403fc
Anthony Sneed <tony@tonysneed.com>
2016-01-15 15:24:00 +0100

    Set up debugging without gulp.

---
+++ src/js/HelloWorld.js
@@ -0,0 +1,12 @@
+var Startup = (function () {
+    function Startup() {
+    }
+    Startup.main = function () {
+        console.log("Hello World");
+        return 0;
+    };
+    return Startup;
+})();
+exports.Startup = Startup;
+Startup.main();
+//# sourceMappingURL=HelloWorld.js.map
